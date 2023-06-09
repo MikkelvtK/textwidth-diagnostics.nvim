cfb14193e0473443bb1f240f1f2dc94aa0667839
MikkelvtK <mvtklooster@hotmail.com>
2023-06-09 16:46:08 +0200

    Only load on files

--- plugin/textwidth-diagnostics.lua
+++ plugin/textwidth-diagnostics.lua
@@ -2,19 +2,20 @@ if vim.g.loaded_textwidth_diagnostics == 1 then
   return
 end
 
+local diag_textwidth = require("textwidth-diagnostics")
 local group = vim.api.nvim_create_augroup("TextwidthDiagnosticsGroup", {
   clear = true
 })
 vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave", }, {
   group = group,
   callback = function()
-    if vim.fn.expand("<amatch>") then
-      vim.schedule(require("textwidth-diagnostics").refresh)
+    if vim.fn.expand("<afile>") ~= "" then
+      vim.schedule(diag_textwidth.refresh)
     end
   end,
 })
 
 vim.api.nvim_create_user_command("TextwidthDiagnosticsToggle",
-  require("textwidth-diagnostics").toggle, {})
+  diag_textwidth.toggle, { desc = "Toggle Textwidth Diagnostics", })
 
 vim.g.loaded_textwidth_diagnostics = 1
